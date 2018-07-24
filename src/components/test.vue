9cb2427494424d3d0d4b3159db5f252c1d4e0225
GUANMING <3210565968@qq.com>
2018-07-24 20:50:04 +0800

    A vue.js project

---
+++ src/components/test.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <!--<div>
+        this is a test page.
+        <router-link to="HelloWorld"><button class="btn">click to the HelloWorld page.</button></router-link>
+    </div>-->
+    <section class="hero is-full-height ">
+        <div class="hero-body">
+            <div class="container">
+                <figure class="center" style="center">
+                    <img src='../assets/logo.png' style="width=40px">
+                </figure>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+}
+</script>
+<style>
+.center{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
